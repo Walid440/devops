07391f055a48ca991af64838c3cae6882687f3e0
walid <walid.gammoudi@esprit.tn>
2022-12-08 12:43:21 +0100

    test

--- src/main/java/tn/esprit/spring/ExamThourayaS2Application.java
+++ src/main/java/tn/esprit/spring/ExamThourayaS2Application.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
 @SpringBootApplication
 @EnableScheduling
 @EnableAspectJAutoProxy
+@EnableSwagger2WebMvc
 public class ExamThourayaS2Application {
 
 	public static void main(String[] args) {
